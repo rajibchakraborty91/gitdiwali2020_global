780f56b8059f729a95d51db8058c6d58bd0219cf
Rajib <Rajib@DESKTOP-MSUCRPV>
2020-11-14 21:32:16 +0530

    new class added for git testing

---
+++ java8test/src/StringTesting.java
@@ -0,0 +1,20 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class StringTesting {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x,a,b;
+		
+	
+	System.out.println("testing ---- git and push");
+
+}
+}
